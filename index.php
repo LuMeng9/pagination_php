50c3001c160620f6e6b41aa9dd242da4485bd564
LuMeng9 <64152971+LuMeng9@users.noreply.github.com>
2020-05-14 21:07:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>To make pagination in php</title>
+</head>
+
+<body>
+    <?php
+    // connect to database
+    $con = mysqli_connect('localhost', 'root', '');
+    mysqli_select_db($con, 'pagination');
+
+    // define how many results you want per page
+    $result_per_page = 10;
+
+    // find out the number of results stored in database
+    $sql = "SELECT * from myDB"; // myDB : It's my database name.
+    $result = mysqli_query($con, $sql);
+    $number_of_results = mysqli_num_rows($result);
+
+    // determine number of total pages available
+    $number_of_pages = ceil($number_of_results / $result_per_page);
+
+    // determine which page number visitor is currently on
+    if (!isset($_GET['page'])) {
+        $page = 1;
+    } else {
+        $page = $_GET['page'];
+    }
+
+    // determine the sql LIMIT starting number for the results on the displaying page
+    $this_page_first_result = ($page - 1) * $result_per_page;
+
+    // retrieve selected results from database and display them on page
+    $sql = "SELECT * FROM myDB LIMIT" . $this_page_first_result . "," . $result_per_page;
+    $result = mysqli_query($con, $sql);
+
+    while ($row = mysqli_fetch_array($result)) {
+        echo $row['id'] . ' ' . $row['alphabet'] . '<br>';
+    }
+
+    // display the links to the pages
+    for ($page = 1; $page <= $number_of_pages; $page++) {
+        echo "<a href='index.php?page=" . $page . "'>" . $page . "</a>";
+    }
+    ?>
+</body>
+
+</html>
